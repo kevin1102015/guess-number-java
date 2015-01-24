391b611b9c3e566f28396def1c0f053ec70a59a3
周勇 <1059981087@qq.com>
2015-01-24 14:11:39 +0800

    make code better

--- java/maven/src/main/java/CompareNumber.java
+++ java/maven/src/main/java/CompareNumber.java
@@ -3,20 +3,15 @@
  */
 public class CompareNumber {
     public String getTips(String input, String answer) {
-        int aCount = 0;
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == answer.charAt(i)) {
-                aCount++;
-            }
-        }
-
-        int containsCount = 0;
+        int aCount = 0, containsCount = 0;
         for (int i = 0; i < input.length(); i++) {
             if (answer.contains(String.valueOf(input.charAt(i)))) {
                 containsCount++;
             }
+            if (input.charAt(i) == answer.charAt(i)) {
+                aCount++;
+            }
         }
-
         return aCount + "A" + (containsCount - aCount) + "B";
     }
 
--- java/maven/src/test/java/CompareNumberTest.java
+++ java/maven/src/test/java/CompareNumberTest.java
@@ -11,45 +11,31 @@ public class CompareNumberTest {
         //given
         String input = "1234";
         String answer = "1234";
-
-        CompareNumber compareNumber = new CompareNumber();
-        String result = compareNumber.getTips(input, answer);
+        String result = new CompareNumber().getTips(input, answer);
         assertThat(result).isEqualTo("4A0B");
     }
-
     @Test
     public void should_be_return_0A0B_when_given_input_is_1234_and_answer_is_5678() {
         //given
         String input = "1234";
         String answer = "5678";
-
-        CompareNumber compareNumber = new CompareNumber();
-        String result = compareNumber.getTips(input, answer);
-
+        String result = new CompareNumber().getTips(input, answer);
         assertThat(result).isEqualTo("0A0B");
     }
-
     @Test
     public void should_be_return_0A4B_when_given_input_is_1234_and_answer_is_4321() {
         //given
         String input = "1234";
         String answer = "4321";
-
-        CompareNumber compareNumber = new CompareNumber();
-        String result = compareNumber.getTips(input, answer);
-
+        String result = new CompareNumber().getTips(input, answer);
         assertThat(result).isEqualTo("0A4B");
     }
-
     @Test
     public void should_be_return_1A1B_when_given_input_is_1234_and_answer_is_1467() {
         //given
         String input = "1234";
         String answer = "1467";
-
-        CompareNumber compareNumber = new CompareNumber();
-        String result = compareNumber.getTips(input, answer);
-
+        String result = new CompareNumber().getTips(input, answer);
         assertThat(result).isEqualTo("1A1B");
     }
 
