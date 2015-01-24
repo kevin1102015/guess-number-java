d45f1a4691b61592f9d3c3b63b0ad6f6a8277f9f
周勇 <1059981087@qq.com>
2015-01-24 11:51:46 +0800

    add CompareNumberTest and CompareNumber,test answer is right

---
+++ java/maven/src/main/java/CompareNumber.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zhouyong on 1/24/15.
+ */
+public class CompareNumber {
+    public String getTips(String input, String answer) {
+        if (input.equals(answer)) {
+            return "4A0B";
+        }
+        return null;
+    }
+}
---
+++ java/maven/src/test/java/CompareNumberTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Created by zhouyong on 1/24/15.
+ */
+public class CompareNumberTest {
+    @Test
+    public void should_be_return_4A0B_when_given_input_is_1234_and_answer_is_1234() {
+        //given
+        String input = "1234";
+        String answer = "1234";
+
+        CompareNumber compareNumber = new CompareNumber();
+        String result = compareNumber.getTips(input, answer);
+        assertThat(result).isEqualTo("4A0B");
+    }
+}
