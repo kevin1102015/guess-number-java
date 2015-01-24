3b3e0cea736d8dc8400dd7cd39bbbeb616ead7ea
周勇 <1059981087@qq.com>
2015-01-24 15:37:09 +0800

    add test case should not repeat in 3 times and implement

--- java/maven/src/main/java/NumberGenerator.java
+++ java/maven/src/main/java/NumberGenerator.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by zhouyong on 1/24/15.
  */
 public class NumberGenerator {
-    public String genetate() {
-        Random random = new Random();
+    private final ArrayList<String> historyGenerateDigits;
+    private Random random;
+
+    public NumberGenerator(Random random) {
+        this.random = random;
+        this.historyGenerateDigits = new ArrayList<String>();
+    }
+
+    public String generate() {
+        String number = generateOnce();
+        while (historyGenerateDigits.contains(number)) {
+            number = generateOnce();
+        }
+        historyGenerateDigits.add(number);
+        return number;
+    }
+
+    private String generateOnce() {
         StringBuilder number = new StringBuilder();
         while (number.length() < 4) {
             String digit = String.valueOf(random.nextInt(10));
--- java/maven/src/test/java/NumberGeneratorTest.java
+++ java/maven/src/test/java/NumberGeneratorTest.java
@@ -1,7 +1,12 @@
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Random;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.fest.assertions.api.Assertions.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by zhouyong on 1/24/15.
@@ -9,13 +14,13 @@ import static org.fest.assertions.api.Assertions.fail;
 public class NumberGeneratorTest {
     @Test
     public void should_generate_4_digits_string(){
-        String number = new NumberGenerator().genetate();
+        String number = new NumberGenerator(new Random()).generate();
         assertThat(number.length()).isEqualTo(4);
     }
 
     @Test
     public void should_generate_number(){
-        String number = new NumberGenerator().genetate();
+        String number = new NumberGenerator(new Random()).generate();
         try {
             Integer.parseInt(number);
         } catch (NumberFormatException e) {
@@ -25,9 +30,27 @@ public class NumberGeneratorTest {
     
     @Test
     public void should_generate_no_duplicate_digits_string(){
-        String number = new NumberGenerator().genetate();
+        String number = new NumberGenerator(new Random()).generate();
         for (int i = 0; i < number.length(); i++) {
             assertThat(number.indexOf(number.charAt(i))).isEqualTo(number.lastIndexOf(number.charAt(i)));
         }
+    }
+
+    @Test
+    public void should_not_repeat_3_times() {
+        Random random = mock(Random.class);
+        given(random.nextInt(10))
+                .willReturn(1, 2, 3, 4)
+                .willReturn(1, 2, 3, 4)
+                .willReturn(2, 3, 4, 5)
+                .willReturn(3, 4, 5, 6);
+        NumberGenerator numberGenerator = new NumberGenerator(random);
+        HashSet<String> numbers = new HashSet<String>();
+
+        numbers.add(numberGenerator.generate());
+        numbers.add(numberGenerator.generate());
+        numbers.add(numberGenerator.generate());
+
+        assertThat(numbers.size()).isEqualTo(3);
     }
 }
