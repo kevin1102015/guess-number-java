95ac7d57436714f8501baf3c72db8edb076b13c5
周勇 <1059981087@qq.com>
2015-01-24 14:41:32 +0800

    use real logic replace quick implement

--- java/maven/src/main/java/NumberGenerator.java
+++ java/maven/src/main/java/NumberGenerator.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 /**
  * Created by zhouyong on 1/24/15.
  */
 public class NumberGenerator {
     public String genetate() {
-
-        return "1234";
+        Random random = new Random();
+        StringBuilder number = new StringBuilder();
+        while (number.length() < 4) {
+            String digit = String.valueOf(random.nextInt(10));
+            if (number.indexOf(digit) == -1) {
+                number.append(digit);
+            }
+        }
+        return number.toString();
     }
 }
